f396cd9774c824924b0fc1fe65a07974ff5524d0
Programming Wormhole <thennvvirus@gmail.com>
2024-10-19 09:45:23 -0700

    Setting Ui Updated

--- lib/views/setting/setting.dart
+++ lib/views/setting/setting.dart
@@ -7,7 +7,7 @@ class SettingView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Github Test From First User'),
+        title: const Text('Hello World'),
       ),
     );
   }
